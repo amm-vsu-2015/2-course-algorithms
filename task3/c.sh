4038ecb01ff33465c28d6c0c4b2d7f9988d30a14
pavbox <pavboxdev@gmail.com>
2017-04-24 22:27:41 +0300

    task Queen was initialized

---
+++ task3/c.sh
@@ -0,0 +1,10 @@
+# alternate compile
+# g++ -o task1 task1.cpp
+# ./task1
+
+# compile and start program instruction (for mac or linux)
+# exec by
+# $ sh c.sh
+
+c++ task3.cpp -o task3 -std=c++11 -stdlib=libc++
+./task3
