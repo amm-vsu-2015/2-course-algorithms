f3bbfffe8e622733e19aa9c0f072e68af2e78c7e
pavbox <pavboxdev@gmail.com>
2017-09-26 17:36:42 +0300

    queen task: recursive

--- task3/task3.cpp
+++ task3/task3.cpp
@@ -1,21 +1,89 @@
-
-/**
-
-  Задача о восьми ферзях:
-  1. Узнать количество положение ферзей, при которых они не бьют друг друга.
-  2. Выбрать одну пару компинаций (т.к. положения будут зеркальными)
-  3. Положения зеркальны относительно горизонтальной плоскости.
-
-*/
-
 #include <iostream>
 
-using namespace std;
+const int SIZE = 8;
 
-int main() {
-  int stop;
+int board[SIZE][SIZE];
+int results_count = 0;
 
-  std::cout << "Chess Queen initial." << '\n';
-  cin >> stop;
-  return 0;
+
+void showBoard()
+{
+    for(int a = 0; a < SIZE; ++a)
+    {
+        for(int b = 0; b < SIZE; ++b)
+        {
+            std::cout << ((board[a][b]) ? "Q " : ". ");
+        }
+        std::cout << '\n';
+    }
+}
+
+
+bool tryQueen(int row, int column)
+{
+    for(int i = 0; i < column; ++i)
+    {
+        if(board[row][i])
+        {
+            return false;
+        }
+    }
+
+    for(int i = 1; i <= column && row-i >= 0; ++i)
+    {
+        if(board[row-i][column-i])
+        {
+            return false;
+        }
+    }
+
+    for(int i = 1; i <= column && row+i < SIZE; i++)
+    {
+        if(board[row+i][column-i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
+
+void setQueen(int column, int &counter, int solution)
+{
+    if (column == SIZE) counter++;
+
+    if ((counter == solution || counter == 92 - solution + 1) && column == SIZE)
+    {
+        showBoard();
+        std::cout << '\n';
+        return;
+    }
+
+    for(int i = 0; i < SIZE; ++i)
+    {
+        if(tryQueen(i, column))
+        {
+            board[i][column] = 1;
+            setQueen(column + 1, counter, solution);
+            board[i][column] = 0;
+        }
+    }
+
+    return;
+}
+
+int main()
+{
+    int counter = 0;
+    int solution = 1;
+
+    std::cout << "Write number of solution (1 of 46): ";
+    std::cin >> solution;
+
+    std::cout << "Solution number: " << solution << '\n';
+    setQueen(0, counter, solution);
+
+    return 0;
 }
